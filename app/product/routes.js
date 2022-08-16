4cd0a5c6de79edc50ca0f5b8cb5c5acf2df7666f
DandanIsmail42 <dandanismail42@gmail.com>
2022-08-16 04:04:31 -0700

    first

---
+++ app/product/routes.js
@@ -0,0 +1,55 @@
+const router = require('express').Router();
+const multer  = require('multer');
+const upload = multer({ dest: '../../uploads/' });
+const productControler = require('./controler');
+
+const fs = require('fs');
+
+
+
+router.get('/product', productControler.index);
+router.get('/product/:id', productControler.view);
+router.post('/product/', upload.single('image'), productControler.store);
+
+
+router.get('/:category/:tag', (req, res) => {
+    const { category, tag} = req.params;
+    res.json({
+        category, tag
+    });
+});
+
+router.post('/prod/', upload.single('image'), (req, res) => {
+    const {name, price, stock, status} = req.body;
+    const image = req.file;
+    if(image) {
+        const target = path.join(__dirname, '../../uploads', image.originalname);
+        fs.renameSync(image.path, target);
+        res.json({
+            name,
+            price,
+            stock,
+            status,
+            image
+        })
+        res.sendFile(target);
+    }
+});
+  
+module.exports = router;
+
+
+
+// router.post('/prod/', upload.single('image'), (req, res) => {
+    //     const {name, price, stock, status} = req.body;
+    //     console.log(req.file);
+    //      res.json({
+    //         name,
+    //         price,
+    //         stock,
+    //         status,
+    //         image,
+    //         author
+    //      });
+        
+    // });
