76ee7ac0b49d67d97019beab6b9385db3a8053a6
DandanIsmail42 <dandanismail42@gmail.com>
2022-10-06 15:19:22 +0700

    Modify npm start and routes product_v2

--- app/product_v2/routes.js
+++ app/product_v2/routes.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const controlerV2 = require('./controller');
 
-router.get('/product', controlerV2.index);
-router.get('/product/:id', controlerV2.view);
-router.post('/product', controlerV2.store);
-router.put('/product/:id', controlerV2.update);
-router.delete('/product/:id', controlerV2.deleteProduct);
+router.get('/daftarmahasiswa', controlerV2.index);
+router.get('/daftarmahasiswa/:id', controlerV2.view);
+router.post('/daftarmahasiswa', controlerV2.store);
+router.put('/daftarmahasiswa/:id', controlerV2.update);
+router.delete('/daftarmahasiswa/:id', controlerV2.deleteProduct);
 
 
 
